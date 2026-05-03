0ee78c4addb0dd16a9262a2aa88a43b8623b5f0f
Shaquille Rashaun Sahl Tamrin <mathqna.project@gmail.com>
2026-05-04 00:07:12 +0800

    first commit

---
+++ database/seeders/RoleAdminSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+
+class RoleAdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $adminRole = Role::create(['name' => 'admin']);
+        $dosenRole = Role::create(['name' => 'dosen']);
+        $mahasiswaRole = Role::create(['name' => 'mahasiswa']);
+
+        $user = User::create([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'phone' => '081234567890',
+            'password' => 'admin',
+        ]);
+
+        $user->assignRole($adminRole);
+    }
+}
