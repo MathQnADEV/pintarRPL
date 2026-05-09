0146fc0d2af3d618cfa2513b04edd99b2490a953
Aditya Sulaiman M <adityasulaiman6@gmail.com>
2026-05-09 23:59:41 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,19 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/login', function () {
+    return view('login');
 });
 
+Route::get('/mahasiswa', function () {
+    return view('dashboard-mahasiswa');
+});
 
+Route::get('/dosen', function () {
+    return view('dashboard-dosen');
+});
+
+Route::get('/materi', function () { return view('materi'); });
+Route::get('/kuis', function () { return view('kuis'); });
+Route::get('/progres', function () { return view('progres'); });
+Route::get('/profil', function () { return view('profil'); });
